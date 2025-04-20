f8e073f10359c6b8e7cac93efdeded9373492e1e
karmokarjay <karmokarjay@gmail.com>
2025-04-20 22:30:03 +0530

    Updated code combined with package.

--- shagenerator/src/main/java/com/jay/shagenerator/GenerateSHAResult.kt
+++ shagenerator/src/main/java/com/jay/shagenerator/GenerateSHAResult.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Base64
 import android.util.Log
+import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 
 object GenerateSHAResult {
@@ -27,10 +28,18 @@ object GenerateSHAResult {
                 packageInfo.signatures
             }
 
-            val signature = signatures?.getOrNull(0)
             val md = MessageDigest.getInstance("SHA-256")
-            val sha = signature?.toByteArray()?.let { md.digest(it) }
-            Base64.encodeToString(sha, Base64.NO_WRAP)
+            signatures?.getOrNull(0)?.toByteArray()?.let { md.update(it) }
+            val sha256 = md.digest()
+            Log.d("Sign sha256 before ::", "$sha256")
+            Log.d("Sign packageName ::", packageName)
+
+            val applicationPackageByteArray = packageName.toByteArray(StandardCharsets.UTF_8)
+            val shaMixedPackage = sha256 + applicationPackageByteArray
+            Log.d("Sign mixedSha256 ::", "$shaMixedPackage")
+//            Log.d("Sign hex array ::","${shaMixedPackage.toHexArray().contentToString}")
+
+            Base64.encodeToString(shaMixedPackage, Base64.NO_WRAP)
         } catch (e: Exception) {
             Log.e("SHA256", "Error getting SHA-256", e)
             null
